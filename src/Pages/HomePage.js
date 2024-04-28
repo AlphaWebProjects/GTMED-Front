96d7c86b9c58dbd791e080da3581d2430fd4fcf0
Pedro <malpvpandtextures@gmail.com>
2024-04-28 14:49:41 -0300

    all cursos

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import Home from '../Components/HomePage/Home/Home';
-
+import TodosCursos from '../Components/HomePage/Home/TodosCursos';
 export default function HomePage() {
     return (
         <Container>
             <Header />
             <Home />
+            <TodosCursos />
             <Footer />
         </Container>
     );
